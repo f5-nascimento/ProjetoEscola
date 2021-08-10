3c8a2690320b575799c9ca3b0e8e928c5262214a
f5-nascimento <nascimento.conceito@gmail.com>
2021-08-09 23:58:03 -0300

    Iniciando Repositório

---
+++ src/escola/Escola.java
@@ -0,0 +1,29 @@
+
+package escola;
+
+
+public class Escola {
+
+    
+    public static void main(String[] args) {
+        
+       
+       
+        String nome = "Felippe Nascimento";
+        int idade = 34;
+        String curso = "Análise de Sistemas";
+        double nota = 8.2;
+        boolean status = true;
+
+     
+        System.out.println("Aluno: "+ nome + "\n"   +  
+                           "Idade: "+  idade + "\n" +  
+                           "Curso: "+ curso + "\n"  +  
+                           "Nota: " + nota + "\n"   +
+                           "Status" + status);
+        
+    }
+    
+}
+        
+   
