19273f2b7f52824bad0b1c8adde28881e54f51fa
f5-nascimento <nascimento.conceito@gmail.com>
2021-08-12 22:44:32 -0300

    Atualização

--- src/escola/Escola.java
+++ src/escola/Escola.java
@@ -1,6 +1,7 @@
 
 package escola;
 
+//IMPORTAÇÃO DA CLASSE SCANNER
 import java.util.Scanner;
 
 
@@ -17,22 +18,23 @@ public class Escola {
         double nota;
         boolean status;
         
-         Scanner ler = new Scanner (System.in, "ISO-8859-1");
+        //UTILIZAÇÃO DA CLASSE SCANNER
+        Scanner ler = new Scanner (System.in, "ISO-8859-1");
          
-         System.out.print("Digite o nome do(a) aluno(a): ");
-         nome = ler.nextLine();
+        System.out.print("Digite o nome do(a) aluno(a): ");
+        nome = ler.nextLine();
          
-         System.out.print("Digite o curso do(a) aluno(a): ");
-         curso = ler.nextLine();
+        System.out.print("Digite o curso do(a) aluno(a): ");
+        curso = ler.nextLine();
          
-         System.out.print("Digite a idade do(a) aluno(a): ");
-         idade = ler.nextInt();
+        System.out.print("Digite a idade do(a) aluno(a): ");
+        idade = ler.nextInt();
          
-         System.out.print("Digite a nota do(a) aluno(a): ");
-         nota = ler.nextDouble();
+        System.out.print("Digite a nota do(a) aluno(a): ");
+        nota = ler.nextDouble();
          
-         System.out.print("Digite o status do(a) aluno(a): ");
-         status = ler.nextBoolean();
+        System.out.print("Digite o status do(a) aluno(a): ");
+        status = ler.nextBoolean();
 
      
         System.out.println("Aluno: "+ nome + "\n"   +  
